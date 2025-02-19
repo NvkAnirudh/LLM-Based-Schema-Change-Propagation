8a1dc297a7f9637d5782f506e71cf4dbd5ddd72a
Anirudh Nuti <nuti.krish4@gmail.com>
2025-02-19 13:54:33 -0500

    Update upstream_schema_changes.sql

--- upstream_schema_changes.sql
+++ upstream_schema_changes.sql
@@ -1,1 +1,14 @@
+select * from bronze_fraud_transactions;
 
+-- Alter the bronze table to add a new column for device type
+ALTER TABLE bronze_fraud_transactions 
+ADD COLUMN device_type VARCHAR(50);
+
+-- Update existing records based on the transaction source
+UPDATE bronze_fraud_transactions
+SET device_type = CASE 
+    WHEN transaction_source = 'web' THEN 'Desktop'
+    WHEN transaction_source = 'mobile' THEN 'Smartphone'
+    WHEN transaction_source = 'ATM' THEN 'Kiosk'
+    ELSE 'Unknown'
+END;
